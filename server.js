993748a3e9c22f642f80db768076c5d517054756
JuanQuezada <ju.quezada.c@gmail.com>
2022-02-28 11:49:16 -0300

    terminado

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const bodyParser = require("body-parser");
 const expressFileUpload = require('express-fileupload');// 1. Integrar express-fileupload a Express.(1 Punto)
+const fs = require('fs');
 
 app.listen(3000, () => {
     console.log("El servidor está inicializando en el puerto 3000");
 });
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.use(express.static("public"));
 
 app.use(expressFileUpload({// 2. Definir que el límite para la carga de imágenes esde 5MB.(2 Puntos)
@@ -19,23 +21,30 @@ app.use(expressFileUpload({// 2. Definir que el límite para la carga de imágen
 );
 
 app.get("/", (_, res) => {
-    res.sendFile(__dirname + "/formulario.html");
-    });
-    
+    res.sendFile(__dirname + "/public/formulario.html");
+});
+
 // 4. Crear una ruta POST /imagen que reciba y almacene una imagen en una carpeta pública del servidor. Considerar que 
 //el formulario envía un payload con una propiedad “position”, que indica la posición del collage donde se deberá mostrar la imagen. (3 Puntos)    
 app.post("/imagen", (req, res) => {
     const { target_file } = req.files;
     const { posicion } = req.body;
-    target_file.mv(`${__dirname}/public/imagenes/${posicion}.jpg`, (err) => {
-    res.send("Archivo cargado con éxito");
+    target_file.mv(`${__dirname}/public/imgs/imagen-${posicion}.jpg`, (err) => {
+    // res.send("Archivo cargado con éxito");
+    res.redirect('/collage')
     });
 });
+
+app.get("/collage", (_, res) => {
+    res.sendFile(__dirname + "/public/collage.html");
+});
+
 // 5. Crear una ruta GET /deleteImg/:nombre que reciba como parámetro el nombre de
 // una imagen y la elimine de la carpeta en dondeestánsiendoalojadaslasimágenes.    
-app.delete("/imagen/:nombre", (req, res) => {
+app.get("/deleteImg/:nombre", (req, res) => {
     const { nombre } = req.params;
-    fs.unlink(`${__dirname}/public/imagenes/${nombre}.jpg`, (err) => {
+    fs.unlink(`${__dirname}/public/imgs/${nombre}`, (err) => {
+        if (err) throw err
     res.send(`Imagen ${nombre} fue eliminada con éxito`);
     });
 });
